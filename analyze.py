55f019cef42728c402eb9f8bcd6372893cd82f8b
mitbailey <mitbailey99@gmail.com>
2021-12-14 18:46:45 -0500

    Ported ringbuffer class to python.

--- analyze.py
+++ analyze.py
@@ -7,6 +7,142 @@ import sys
 # %%
 # sys.argv = ['', '5', '1']
 # %%
+class RingBuf:
+    data = 0
+    idx = -1
+    full = False
+    dtype = float
+    size = 0
+    pushed = 0
+    
+    def __init__(self, dtype = float):
+        self.dtype = dtype
+        return
+
+    def __init__(self, size, dtype = float):
+        self.dtype = dtype
+        self.size = size
+        self.Initialize(size)
+        return
+
+    def Initialize(self, size):
+        if (size < 1):
+            raise ValueError("Buffer size can not be negative or zero.")
+        self.size = size
+        self.data = np.zeros(size, dtype = self.dtype)
+        self.pushed = 0
+        self.idx = -1
+        self.full = False
+        return
+
+    def HasData(self):
+        return self.size > 0
+
+    def IsFull(self):
+        return self.full
+
+    def GetIndex(self):
+        return self.idx
+
+    def __getitem__(self, key):
+        if not self.HasData():
+            raise Exception("Buffer not initialized.")
+        if isinstance(key, slice):
+            indices = key.indices((self.size))
+            stride = -1 if indices[2] == None else -indices[2]
+            start = self.GetIndex() - indices[0]
+            while start < 0:
+                start = start + self.size
+            stop = self.GetIndex() - indices[1]
+            while stop < 0:
+                stop = stop + self.size
+            print(start, stop, stride)
+            if stop >= start:
+                out = np.concatenate((self.data[start::stride], self.data[-1:stop:stride]))
+                return out
+            else:
+                out = self.data[start:stop:stride]
+                return out
+
+        if key < 0:
+            raise Exception("Index cannot be negative.")
+        if key >= self.size:
+            key = key % self.size
+        index = (self.idx - key + self.size) % self.size
+        return self.data[index]
+    
+    def __setitem__(self, key, val):
+        if not self.HasData():
+            raise Exception("Buffer not initialized.")
+        if key < 0:
+            raise Exception("Index cannot be negative.")
+        if key >= self.size:
+            key = key % self.size
+        index = (self.idx - key + self.size) % self.size
+        self.data[index] = val
+        return
+
+    def push(self, data):
+        if not self.HasData():
+            raise Exception("Buffer not initialized.")
+        if self.idx == self.size - 1:
+            self.full = True
+        self.idx = (self.idx + 1) % self.size
+        self.data[self.idx] = data
+        self.pushed += 1
+        return
+
+    def clear(self):
+        if not self.HasData():
+            return
+        self.idx = -1
+        self.full = False
+        self.data = np.zeros(self.size, dtype = self.dtype)
+        pushed = 0
+        return
+    
+    def GetPushed(self):
+        return self.pushed
+    
+    def GetSize(self):
+        return self.size
+
+# %%
+class GaussFilter:
+    gauss_coeff = 0 
+    size = 0
+
+    def __init__(self, size, sigma, dtype = float):
+        if size < 2:
+            raise Exception("Gaussian filter size cannot be < 2.")
+        self.size = size
+        xr = np.arange(size, dtype = dtype)
+        self.gauss_coeff = np.exp(-xr*xr/sigma/sigma)
+        return
+    
+    def ApplyFilter(self, buf: RingBuf):
+        if not buf.HasData():
+            raise Exception("Ring buffer not initialized")
+        
+        num = buf.GetPushed() if buf.GetPushed() <= self.size else self.size
+        coeffs = np.sum(self.gauss_coeff[0:num])
+        val = np.sum(self.gauss_coeff[0:buf.GetIndex()+1] * buf.data[buf.GetIndex()::-1])
+        num -= buf.GetIndex()
+        if buf.IsFull() and num > 0:
+            val += np.sum(self.gauss_coeff[buf.GetIndex()+1:buf.GetIndex() + num] * buf.data[-1:buf.GetIndex():-1])
+        return val / coeffs
+# %%
+buf = RingBuf(10)
+dat = RingBuf(10)
+ftr = GaussFilter(10, 2)
+for i in range(15):
+    buf.push(float(i))
+    dat.push(i)
+    buf[0] = ftr.ApplyFilter(buf)
+plt.plot(np.flip(buf[:]))
+plt.plot(np.flip(dat[:]))
+plt.show()
+# %%
 if len(sys.argv) != 3:
     print("Invocation: %s <Order> <Cutoff Frequency>\n\n"%(sys.argv[0]))
     sys.exit(0)
